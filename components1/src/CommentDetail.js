4d9867f645de5a6712539ddcf2377225c7cc665a
BrianArchibald <brianallenarchibald@gmail.com>
2018-12-18 19:56:57 -0600

    reusable component progress

--- components1/src/CommentDetail.js
+++ components1/src/CommentDetail.js
@@ -1,25 +1,27 @@
 import React from 'react';
-import faker from 'faker';
+
 
 // This is a component
-const CommentDetail = () => {
+const CommentDetail = (props) => {
 	return (
 		<div className="comment">
 			<a href="/" className="avatar">
-				<img alt="avatar" src={faker.image.avatar()} />
+				<img alt="avatar" src={props.avatar} />
 			</a>
 			<div className="content">
 				<a href="/" className="author">
-					Sam
+					{props.author} 
 				</a>
 				<div className="metadata">
-					<span className="date">Today at 6:00PM</span>
+					<span className="date">{props.timeAgo}</span>
 				</div>
-				<div className="text">Nice blog post!</div>
+				<div className="text">{props.blogText}</div>
 			</div>
 		</div>
 	);
 };
+
+//{props.author} is looking at my index file and seeing what prop I set as author on index.
 
 // Export this file available to rest of project
 export default CommentDetail;
--- components1/src/index.js
+++ components1/src/index.js
@@ -1,18 +1,44 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import faker from 'faker';
+import ReactDom from 'react-dom';
+import facker from 'faker';
 import CommentDetail from './CommentDetail';  // Its in the same folder
+import ApprovalCard from './ApprovalCard';
 
 // Whenever showing a component inside another component, you use JSX <CommentDetail /> not {CommentDetail}
+// Passing props  <CommentDetail author="Sam" />  author is name of prop, Sam is value of prop.  Sam could be {jsval}.
+
 
 const App = () => {
 	return (
 		<div className="ui container comments">
-			<CommentDetail /> 
-			<CommentDetail />
-			<CommentDetail />
-			<CommentDetail />
-			<CommentDetail />
+			<ApprovalCard>Are your sure?</ApprovalCard>
+			
+			<ApprovalCard>
+				<CommentDetail 
+					author="Sam" 
+					blogText="Nice post" 
+					timeAgo="Today at 4:45 PM"
+					avatar={faker.image.avatar()} 
+				/>
+			</ApprovalCard> 
+
+			<ApprovalCard>
+				<CommentDetail 
+					author="Alex" 
+					blogText="Nice" 
+					timeAgo="Today at 1:45 PM"
+					avatar={faker.image.avatar()} 
+				/> 
+			</ApprovalCard>
+
+			<ApprovalCard>
+				<CommentDetail 
+					author="Jane" 
+					blogText="Great" 
+					timeAgo="Today at 12:45 PM"
+					avatar={faker.image.avatar()} 
+				/> 
+			</ApprovalCard>
 		</div>
 	);
 };
